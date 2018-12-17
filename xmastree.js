958f05817c5abb71c87b4ea56854e78ca659f4c9
Héctor <hector.roma@gmail.com>
2018-12-17 21:11:03 +0100

    Clean version

--- xmastree.js
+++ xmastree.js
@@ -1,1 +1,1 @@
-var xmastree = a => '\n' + [R = a, ''][1].padStart(R + 1, '0').split('').map((a, b) => (b == 0 ? '*' : a).repeat((s = b > 1 ? b - 1 : 0) * 2 + 1).padStart(R + s, ' ')).join('\n')
+var xmastree=a=>'\n'+[R=a,''][1].padStart(R+1,'0').split('').map((a,b)=>(b==0?'*':a).repeat((s=b>1?b-1:0)*2+1).padStart(R+s,' ')).join('\n')
